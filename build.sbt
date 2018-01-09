a5e7b339ef76d3be0d8c9e2895aaadd72abd3175
AiR <ai535@ya.ru>
2018-01-09 22:25:05 +0300

    Add checking already downloaded files Delete any multithreading

--- build.sbt
+++ build.sbt
@@ -12,11 +12,6 @@ resolvers += "Sonatype OSS Snapshots" at
 libraryDependencies +=
   "com.storm-enroute" %% "scalameter-core" % "0.8.2"
 
+libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.2.3"
+
 libraryDependencies += "io.monix" %% "monix" % "2.3.0"
-
-resolvers ++= Seq(Resolver.sbtPluginRepo("releases"), Resolver.sbtPluginRepo("snapshots"))
-
-libraryDependencies ++= Seq(
-  // notice the double %% here
-  "com.eed3si9n" %% "sbt-assembly" % "0.13.0"
-)
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,20 +4,17 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.{Document, Element}
 
 import scala.collection.JavaConverters._
-import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.util.Try
-import scala.concurrent.forkjoin.ForkJoinPool
 import scala.io.Source.fromFile
 
 object Main extends App {
 
   def writeToFile(path: String, data: String): Unit = {
-    //    println(path)
+    println(path)
     val pw: BufferedWriter = new BufferedWriter(
       new OutputStreamWriter(
         new FileOutputStream(path, true),
-        "UTF-8"))
+        "windows-1251"))
     try pw.write(data) finally pw.close()
   }
 
@@ -26,7 +23,7 @@ object Main extends App {
     Jsoup.connect(url).get
   }
 
-  def getTagsNames(): List[String] = {
+  def getTagsNames: List[String] = {
     val tagsUrl = "https://pikabu.ru/tag"
     val tagsTableItemClassName = "b-tag-search-result"
     getPage(tagsUrl)
@@ -55,7 +52,7 @@ object Main extends App {
       .map(_.get)
       .toList
 
-  def addPagesNumberToUrl(url: String, howMuchPostsDownloadFromOneUrl: Int = 10): List[String] = {
+  def getTagPagesWithNumber(url: String, howMuchPostsDownloadFromOneUrl: Int = 10): List[String] = {
     val postsOnEveryPage = 20 // Imperative constant taken from the site
     val maxPageNumber: Int = Math.ceil(howMuchPostsDownloadFromOneUrl.toFloat / postsOnEveryPage).toInt
     (1 to maxPageNumber).map { x => f"$url&page=$x" }.toList
@@ -63,6 +60,7 @@ object Main extends App {
 
   def createDirIfNotExists(path: String): Unit =
     new File(path).mkdirs()
+
 
   def downloadPageToFile(url: String, folderPath: String): Unit = {
     val page = getPage(url)
@@ -73,65 +71,54 @@ object Main extends App {
 
   def deleteFileIfExists(path: String): Unit = {
     val fileTemp = new File(path)
-    if (fileTemp.exists()) {
+    if (fileTemp.exists())
       fileTemp.delete()
-    }
   }
 
   def getDownloadedTags(filePath: String): Iterable[String] =
     fromFile(filePath).getLines().toList
 
-  override def main(args: Array[String]): Unit = {
-    //    val OUTPUT_DIRECTORY = "/Users/Air_Folder/Downloads/CourseWorkTrirdCourse/Output/"
-    //    val OUTPUT_DIRECTORY = args(1)
-    //    val NUMBER_OF_THREADS = args(0)
-    //    print(f"Output dir: $OUTPUT_DIRECTORY\n")
-    //    print(f"Number of threads: $NUMBER_OF_THREADS\n")
-    //    val url = "https://pikabu.ru/best/"
+  def getPostNameByItsUrl(url: String): String =
+    url.split("/").last
 
-    val tagsNames = getTagsNames().sorted
 
-//    println(tagsNames.sortBy(_.length).takeRight(10).map(x => x.length + " : " + x).mkString("\n"))
+  def getFileNameWithoutExtension(file: File): String =
+    file.getName.takeWhile(_ != '.')
 
-    println(f"All tages count: ${tagsNames.size}")
+  def downloadTagPosts(tagName: String, howMuchPostsToDownload: Int, folderPath: String): Unit = {
+    val fullOutFolderPath: File = new File(folderPath + '/' + tagName)
+    if (fullOutFolderPath.isDirectory)
+      if (!fullOutFolderPath.exists())
+        throw new IllegalArgumentException("Provided path is not a directory")
 
-    val downloadedTags = getDownloadedTags("/Users/Air_Folder/jupyter/downloadedTags.txt").toSet
-    println(s"Dropped tags count: ${downloadedTags.size}")
-    val tagsUrl: Map[String, String] = tagsNames.map(x => x -> getTagUrlByTagName(x)).toMap.filter(x => !downloadedTags.contains(x._1))
-    println(s"Tags to download count: ${tagsUrl.size}")
-
-    val tagsPagesUrl: Map[String, List[String]] = tagsUrl.mapValues(addPagesNumberToUrl(_, 100))
-
-    implicit val ec: ExecutionContext = ExecutionContext.fromExecutor(new ForkJoinPool(4))
-    val filePath = "/Users/Air_Folder/Downloads/CourseWorkTrirdCourse/postsLinks.txt"
-    deleteFileIfExists(filePath)
-    val tagsPostsUrl = tagsPagesUrl.map { case (tagName, pageUrl: Seq[String]) =>
-      Future {
-        pageUrl.foreach(pageUrl =>
-          getAllPostsUrlOnPage(getPage(pageUrl)).foreach(x => writeToFile(filePath, f"$tagName\t$x\n")))
-      }
-
+    val alreadyDownloadedPostsName: Set[String] = if (fullOutFolderPath.exists())
+      fullOutFolderPath.listFiles().map(getFileNameWithoutExtension).toSet
+    else Set.empty[String]
+    if (alreadyDownloadedPostsName.size == howMuchPostsToDownload) {
+      println(f"'$tagName' folder already has enough downloaded files. Stop downloading.")
+      return
     }
 
-    //    val pageDownloadFutures = tagsPostsUrl.map(
-    //      _.map {
-    //        _.foreach {
-    //          case (tagName, postsUrl: Seq[String]) =>
-    //            postsUrl.foreach(
-    //              downloadPageToFile(_, OUTPUT_DIRECTORY + tagName)
-    //            )
-    //        }
-    //      }
-    //    )
+    val tagUrl = getTagUrlByTagName(tagName)
+    val pages = getTagPagesWithNumber(tagUrl, howMuchPostsToDownload)
+    val postsUrls = pages.map(getPage).flatMap(getAllPostsUrlOnPage).take(howMuchPostsToDownload)
+    val postsNameToUrl = postsUrls.map(x => getPostNameByItsUrl(x) -> x).toMap
+    val postsToDownload = postsNameToUrl.filterKeys(!alreadyDownloadedPostsName.contains(_)).values
+    postsToDownload.foreach(downloadPageToFile(_, fullOutFolderPath.getAbsolutePath))
+  }
 
+  override def main(args: Array[String]): Unit = {
+    if (args.length < 2) {
+      println("There is no enough console params")
+      return
+    }
 
-    val future = Future.sequence(tagsPostsUrl)
-    Await.ready(future, Duration.Inf)
-    //    val res = Await.result(future, Duration.Inf).flatten.toList.sortBy(_._1)
-    //    val out = res.flatMap { case (tagName, lstOfLinks) => lstOfLinks.sorted.map(tagName + "\t" + _) }.mkString("\n")
+    val everyTagPostsCount = args(0).toInt
+    val outFolderPath = args(1)
 
-    //      .map(x => x._1 + "\t" + x._2.sorted.mkString("\t")).toSet.toList.sorted.mkString("\n")
-    //    writeToFile(filePath, out)
+    val tagsNames = getTagsNames.sorted
+
+    tagsNames.foreach(downloadTagPosts(_, everyTagPostsCount, outFolderPath))
   }
 
 
